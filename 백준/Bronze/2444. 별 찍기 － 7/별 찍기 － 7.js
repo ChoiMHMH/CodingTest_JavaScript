847d3c2493a3767622b11e9d8d6f9e65b8f5cbc2
MinHyeok.Choi <103150252+ChoiMHMH@users.noreply.github.com>
2024-01-20 10:04:55 +0900

    Update 별 찍기 － 7.js

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
@@ -2,16 +2,16 @@ const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/st
 
 //input 5일때
 for(let i = 1; i <= input; i++){
-	let stars = '';
-	// 공백 증가 반복문(4,3,2,1)
-	for(let k = 1; k <= input-i; k++){
-		stars = stars + ' ';
-	}
-	// 별 증가 반복문(1,3,5,7,9)
-	for(let j = 1; j <= i*2-1; j++){ 
-		stars = stars + '*';
-	}
-	console.log(stars);
+  let stars = '';
+  // 공백 증가 반복문(4,3,2,1)
+  for(let k = 1; k <= input-i; k++){
+    stars = stars + ' ';
+  }
+  // 별 증가 반복문(1,3,5,7,9)
+  for(let j = 1; j <= i*2-1; j++){ 
+    stars = stars + '*';
+  }
+  console.log(stars);
 }
 for(let i = 1; i <= input-1; i++){
   let stars = '';
