5fad11c4b7f8e76c55f12ecef31a996f0ba972a2
MinHyeok.Choi <103150252+ChoiMHMH@users.noreply.github.com>
2024-01-24 13:19:57 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.js
@@ -0,0 +1,9 @@
+const input = +require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
+
+
+// function MenOfPassion(A, n) {
+//   let i = (n / 2);
+//   return A[i]; // 코드1
+// }
+console.log(1);
+console.log(0);
