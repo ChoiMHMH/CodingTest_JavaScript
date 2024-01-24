271298b88348c6bd24c0e24d6f99eaee8ba60f80
MinHyeok.Choi <103150252+ChoiMHMH@users.noreply.github.com>
2024-01-25 01:07:37 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
@@ -1,15 +1,6 @@
 const n = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
 
-// function MenOfPassion(A, n) {
-//   let sum = 0;
-//   for (let i = 0; i < n - 1; i++) {
-//       for (let j = i + 1; j < n; j++) {
-//           sum += A[i] * A[j]; // 코드1
-//       }
-//   }
-//   return sum;
-// }
+let me = (n*(n-1))/2;
 
-
-console.log(n*(n-1) - (n-1)*n/2);
+console.log(me);
 console.log(2);
