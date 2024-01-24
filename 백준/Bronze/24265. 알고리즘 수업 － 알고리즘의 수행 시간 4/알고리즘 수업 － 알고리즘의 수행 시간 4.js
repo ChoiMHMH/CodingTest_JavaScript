756951a82fb7d86c9fed413402a9168630a3942d
MinHyeok.Choi <103150252+ChoiMHMH@users.noreply.github.com>
2024-01-25 01:11:19 +0900

    Update 알고리즘 수업 － 알고리즘의 수행 시간 4.js

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
@@ -1,6 +1,16 @@
 const n = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
+// n = 5라고 가정
+// 시간초과
+// function MenOfPassion(n) {
+//   let sum = 0;
+//   for (let i = 0; i < n - 1; i++) {  //0,1,2,3
+//       for (let j = i + 1; j < n; j++) {  //1234 234 34 4
+//           sum++
+//       }
+//   }
+//   return sum;
+// }
+//
 
-let me = (n*(n-1))/2;
-
-console.log(me);
+console.log((n*(n-1))/2);
 console.log(2);
