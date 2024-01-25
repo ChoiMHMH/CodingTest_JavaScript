6fb5f4e09f423129d59550cd8ae673da1ab4ea84
MinHyeok.Choi <103150252+ChoiMHMH@users.noreply.github.com>
2024-01-26 03:01:17 +0900

    Update 분해합.js

--- 백준/Bronze/2231. 분해합/분해합.js
+++ 백준/Bronze/2231. 분해합/분해합.js
@@ -1,22 +1,17 @@
 const N = +require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
 
-let newArr = [];
 
-// mapfn 정의
-// 배열의 모든 element를 숫자로 변경합니다.
-const mapfn = (arg) => Number(arg);
 function splitSum() {
   for (let i = 0; i <= N; i++) {
 
     const str = String(i);
 
-    // 문자열을 배열로 변경합니다 - split()
-    // 배열의 각 element를 숫자로 변경합니다. - map()
-    let newArr = str.split('').map(mapfn);
+    // 문자열을 잘라서 배열로 변경합니다 - split()
+    let newArr = str.split('');
     newArr.push(i);//숫자 본인
 
     let sum = newArr.reduce((accumulator, currentValue) => {
-      return parseInt(accumulator) + parseInt(currentValue)
+      return Number(accumulator) + Number(currentValue)
     }, 0);
 
 
