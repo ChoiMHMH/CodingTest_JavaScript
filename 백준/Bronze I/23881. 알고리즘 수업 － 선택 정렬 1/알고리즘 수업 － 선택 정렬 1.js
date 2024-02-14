c2b6b5bee04398fd2de42be6595b287e1f5565aa
MinHyeok.Choi <103150252+ChoiMHMH@users.noreply.github.com>
2024-02-15 00:23:30 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 224 ms, Memory: 10376 KB -BaekjoonHub

---
+++ 백준/Bronze I/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.js
@@ -0,0 +1,39 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.split(" ").map(Number));
+const answer = input.pop();
+const N = input[0].shift(); //배열의 크기
+const K = input[0].pop(); //교환횟수
+
+function selectionSort(arr) {
+  let count = 0; //교환횟수 세기
+  for (let last = N; last >= 2; last--) {
+    let maxIndex = 0;
+    for (let i = 1; i < last; i++) {
+      if (arr[i] > arr[maxIndex]) {
+        //싹다비교해서 가장 값이 큰 인덱스 찾기
+        maxIndex = i;
+      }
+    }
+    if (last - 1 !== maxIndex) {
+      count++;
+      if (count === K) {
+        return [arr[last - 1], arr[maxIndex]].join(" ");
+      }
+      const temp = arr[last - 1];
+      arr[last - 1] = arr[maxIndex];
+      arr[maxIndex] = temp;
+    }
+  }
+  //교환횟수가 K보다 작으면
+  if (count < K) {
+    return -1;
+  }
+  return arr;
+}
+
+const solution = selectionSort(answer);
+console.log(solution);
