1b89fc8c90414ec0dfb9d8ebf1d7837ec6094568
MinHyeok <103150252+ChoiMHMH@users.noreply.github.com>
2024-06-10 14:01:21 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,14 @@
 function solution(n) {
- let arr =[]
-    for(i=1; i<=n; i++){
-       if(n%i===0) {
-           arr.push(i)
-       }
-   }
-    return arr.reduce((acc,cur)=> acc+cur,0)
+ // let arr =[]
+ //    for(i=1; i<=n; i++){
+ //       if(n%i===0) {
+ //           arr.push(i)
+ //       }
+ //   }
+ //    return arr.reduce((acc,cur)=> acc+cur,0)
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) sum += i
+    }
+    return sum
 }
