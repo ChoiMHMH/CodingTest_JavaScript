5c5dcd912a92b8fe06b181fbd6e27aa9313bb10f
MinHyeok <103150252+ChoiMHMH@users.noreply.github.com>
2024-07-07 23:11:11 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,10 +1,14 @@
+// function solution(s) {
+
+//     const center = Math.floor(s.length / 2);
+//     if(s.length % 2 === 0){
+//         return s.slice(center-1,center+1);
+//     }else{
+//         return s.slice(center,center+1);
+//     }
+
+// }
+
 function solution(s) {
-
-    const center = s.length / 2;
-    if(s.length % 2 === 0){
-        return s.slice(center-1,center+1);
-    }else{
-        return s.slice(center,center+1);
-    }
-
+    return s.substr(Math.ceil(s.length / 2) - 1, s.length % 2 === 0 ? 2 : 1);
 }
