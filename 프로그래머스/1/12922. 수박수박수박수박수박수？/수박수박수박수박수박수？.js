8b06d649c0ad9227eb2fd79881b7c8b7ed055871
MinHyeok <103150252+ChoiMHMH@users.noreply.github.com>
2024-07-09 14:42:55 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,22 +1,24 @@
-function solution(n) {
-    let answer = '';
-    let i = 0;
+// function solution(n) {
+//     let answer = '';
+//     let i = 0;
     
-    while(i < n){
-        if(i === 0){
-            answer += '수';
-            i++;
-            continue;
-        }
-        if(answer.substr(-1) === '박'){
-            answer += '수';
-            i++;
-            continue;
-        }
-        answer += '박';
-        i++;
-        
-    }
-    
+//     while(i < n){
+//         if(i === 0){
+//             answer += '수';
+//             i++;
+//             continue;
+//         }
+//         if(answer.substr(-1) === '박'){
+//             answer += '수';
+//             i++;
+//             continue;
+//         }
+//         answer += '박';
+//         i++;       
+//     }
+//     return answer;
+// }
+function solution(n){
+    const answer = "수박".repeat(n/2+1).slice(0,n);
     return answer;
 }
