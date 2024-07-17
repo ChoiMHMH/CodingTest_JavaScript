999cc0ed7dacb614ab1d9112ddf500d174a27b56
MinHyeok <103150252+ChoiMHMH@users.noreply.github.com>
2024-07-17 10:07:30 +0900

    [Silver V] Title: 최소공배수, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.js
@@ -0,0 +1,21 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+// 최대공약수
+function gcd(a, b) {
+  let r = 0;
+  while (b !== 0) {
+    r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+input.sort((a, b) => b - a);
+// 최소공배수
+console.log((input[0] * input[1]) / gcd(input[0], input[1]));
