d07884ba228ee66e1ff5d707cbe914f9cb23433d
MinHyeok <103150252+ChoiMHMH@users.noreply.github.com>
2024-07-30 14:02:59 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,7 +1,11 @@
-function solution(n) {
-  const trinary = n.toString(3);
-  const triReverse = trinary.split("").reverse().join("");
+// function solution(n) {
+//   const trinary = n.toString(3);
+//   const triReverse = trinary.split("").reverse().join("");
 
-  return parseInt(triReverse, 3);
-}
+//   return parseInt(triReverse, 3);
+// }
 //https://velog.io/@gagaeun/JavaScript-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-3%EC%A7%84%EB%B2%95-%EB%92%A4%EC%A7%91%EA%B8%B0-%EC%A7%84%EC%88%98-%EB%B3%80%ED%99%98 블로그 참고
+
+const solution = (n) => {
+    return parseInt([...n.toString(3)].reverse().join(""), 3);
+}
