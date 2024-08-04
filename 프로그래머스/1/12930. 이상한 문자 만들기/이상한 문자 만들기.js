0c52c6870de42b3775c95338ccdf07eb81b6f4db
MinHyeok <103150252+ChoiMHMH@users.noreply.github.com>
2024-08-04 23:30:57 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,18 +1,23 @@
-function solution(s) {
-    let answer = '';
-    let words = s.split(' ');
-    
-    for (let i = 0; i < words.length; i++) {
-        for(let j = 0; j < words[i].length; j++) {
-            if(j % 2 === 0) {
-                answer += words[i][j].toUpperCase();
-            } else {
-                answer += words[i][j].toLowerCase();
-            }
-        }
-        if (i < words.length -1) {
-            answer += ' ';
-        }
-    }
-    return answer;
+function solution(s){
+  return s.toUpperCase().replace(/(\w)(\w)/g, function(a){return a[0].toUpperCase()+a[1].toLowerCase();})
+
 }
+
+// function solution(s) {
+//     let answer = '';
+//     let words = s.split(' ');
+    
+//     for (let i = 0; i < words.length; i++) {
+//         for(let j = 0; j < words[i].length; j++) {
+//             if(j % 2 === 0) {
+//                 answer += words[i][j].toUpperCase();
+//             } else {
+//                 answer += words[i][j].toLowerCase();
+//             }
+//         }
+//         if (i < words.length -1) {
+//             answer += ' ';
+//         }
+//     }
+//     return answer;
+// }
