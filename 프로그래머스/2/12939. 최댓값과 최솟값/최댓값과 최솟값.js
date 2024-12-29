fff4c211d28f5cb81fd3d9b7ab42204c35a8b14d
MinHyeok <kasamin98@naver.com>
2024-12-29 20:26:46 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const number = s.split(' ').map(Number);
+    
+    const min = Math.min(...number);
+    const max = Math.max(...number);
+
+    return `${min} ${max}`;
+}
