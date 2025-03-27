d9c4fa750f1e561fd50d7ec63c6e20523a9fe1e8
MinHyeok <kasamin98@naver.com>
2025-03-27 09:35:11 +0900

    [level 2] Title: 할인 행사, Time: 22.93 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,30 +1,46 @@
+// function solution(want, number, discount) {
+//     const wantMap = {};
+//     for (let i = 0; i < want.length; i++) {
+//         wantMap[want[i]] = number[i];
+//     }
+
+//     let result = 0;
+
+
+//     for (let i = 0; i <= discount.length - 10; i++) {
+//         const currentWindow = discount.slice(i, i + 10);
+//         const currentMap = {};
+
+//         for (const item of currentWindow) {
+//             currentMap[item] = (currentMap[item] || 0) + 1;
+//         }
+
+//         let isValid = true;
+//         for (const item in wantMap) {
+//             if (currentMap[item] !== wantMap[item]) {
+//                 isValid = false;
+//                 break;
+//             }
+//         }
+
+//         if (isValid) result++;
+//     }
+
+//     return result;
+// }
 function solution(want, number, discount) {
-    const wantMap = {};
-    for (let i = 0; i < want.length; i++) {
-        wantMap[want[i]] = number[i];
-    }
+    let count = 0;
+    for (let i = 0; i < discount.length - 9; i++) {
+        const slice = discount.slice(i, i+10);
 
-    let result = 0;
-
-
-    for (let i = 0; i <= discount.length - 10; i++) {
-        const currentWindow = discount.slice(i, i + 10);
-        const currentMap = {};
-
-        for (const item of currentWindow) {
-            currentMap[item] = (currentMap[item] || 0) + 1;
-        }
-
-        let isValid = true;
-        for (const item in wantMap) {
-            if (currentMap[item] !== wantMap[item]) {
-                isValid = false;
+        let flag = true;
+        for (let j = 0; j < want.length; j++) {
+            if (slice.filter(item => item === want[j]).length !== number[j]) {
+                flag = false;
                 break;
             }
         }
-
-        if (isValid) result++;
+        if (flag) count += 1;
     }
-
-    return result;
+    return count;
 }
