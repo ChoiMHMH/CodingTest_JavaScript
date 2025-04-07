34fb6abad9527112abc19a3ed00df51b55430db3
MinHyeok <kasamin98@naver.com>
2025-04-07 21:29:30 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.93 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,17 @@
+function solution(k, score) {
+    const hallOfFame = [];
+    const result = [];
+
+    for (let i = 0; i < score.length; i++) {
+        hallOfFame.push(score[i]);
+        hallOfFame.sort((a, b) => b - a);
+
+        if (hallOfFame.length > k) {
+            hallOfFame.pop();
+        }
+
+        result.push(hallOfFame[hallOfFame.length - 1]);
+    }
+
+    return result;
+}
