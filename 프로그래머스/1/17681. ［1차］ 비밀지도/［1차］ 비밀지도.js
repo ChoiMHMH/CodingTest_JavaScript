ad22dae65eb19840f093d277bbfad6311c552c18
MinHyeok <kasamin98@naver.com>
2025-07-07 15:36:16 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,10 @@
 function solution(n, arr1, arr2) {
-    return arr1.map((num1, i) => {
-        const num2 = arr2[i];
-        // OR 연산 후 n자리 이진수로 변환, 앞에 0 채우기
-        const binary = (num1 | num2).toString(2).padStart(n, '0');
-        // 1이면 #, 0이면 공백으로 매핑, ai사용
-        return binary.replace(/1/g, '#').replace(/0/g, ' ');
-    });
+  return arr1.map((v, i) => {
+    // 두 지도 각 행을 OR 연산 -> 2진수 변환 -> padStart로 n길이 맞춤 -> #/공백 변환
+    return (v | arr2[i])
+      .toString(2)
+      .padStart(n, '0')
+      .replace(/1/g, '#')
+      .replace(/0/g, ' ');
+  });
 }
